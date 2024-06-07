038329ca6f1ddf9ff15743f6e01e1702c7a623c5
Diana <du397248@gmail.com>
2024-06-07 11:22:07 +0400

    NE RABOTAET DIANA VSE SLOMALA

--- resources/js/Pages/Cabinet/Home/HomePage.vue
+++ resources/js/Pages/Cabinet/Home/HomePage.vue
@@ -1,23 +1,24 @@
 <template>
-
-    <Carousel/>
-<!--    <CardServices/>-->
-    <div class="d-flex flex-column gap-5">
-        <HistoryOfCompany/>
-        <AmountHelp/>
-        <AboutUs/>
-        <WeMap/>
-    </div>
+    <ClientLayout title="Items">
+        <Carousel/>
+        <!--    <CardServices/>-->
+        <div class="d-flex flex-column gap-5">
+            <HistoryOfCompany/>
+            <AmountHelp/>
+            <AboutUs/>
+            <WeMap/>
+        </div>
+    </ClientLayout>
 </template>
 
 <script setup>
+import ClientLayout from "@/Layouts/ClientLayout.vue"
 import Carousel from "@/Pages/Cabinet/Home/Carousel.vue"
 // import CardServices from "@/Pages/Cabinet/Home/CardServices.vue"
 import HistoryOfCompany from "@/Pages/Cabinet/Home/HistoryOfCompany.vue"
 import AmountHelp from "@/Pages/Cabinet/Home/AmountHelp.vue"
 import AboutUs from "@/Pages/Cabinet/Home/AboutUs.vue"
-import WeMap from "@/Pages/Cabinet/Home/WeMap.vue"
-</script>
+import WeMap from "@/Pages/Cabinet/Home/WeMap.vue"</script>
 
 <style scoped>
 
